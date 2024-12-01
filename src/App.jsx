c51394e087fb79c0a9cbdd03a2a2fcc7505c5c67
abhishek8094 <iasabhi41@gmail.com>
2024-12-01 23:49:19 +0530

    section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Navbar from "./components/Navbar";
+import Section from "./components/Section";
 
 function App() {
  
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
     <Navbar/>
+    <Section/>
     </>
   )
 }
